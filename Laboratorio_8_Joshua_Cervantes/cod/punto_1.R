116c7fd89ae661feae24bddba83201a83b4cdee2
afr063426 <saac9907@gmail.com>
2023-11-02 22:28:28 -0600

    feat: function summary kmeans

--- Laboratorio_8_Joshua_Cervantes/cod/punto_1.R
+++ Laboratorio_8_Joshua_Cervantes/cod/punto_1.R
@@ -3,10 +3,10 @@
 require(tidyverse)
 
 #Se programa una funcion para estimar lo soliticitado en el punto 1
-fn_punto_1 <- function(df = iris[,-5], k = 4, nstart = 100, ...){
+fn_punto_1 <- function(df = iris[,-5], k = 2, nstart = 100, ...){
 
     df_kmeans <- data.frame(k = numeric(), 
-        totss = numeric(), 
+        tot.withinss = numeric(), 
         tiempo = numeric())
 
 
@@ -14,40 +14,43 @@ fn_punto_1 <- function(df = iris[,-5], k = 4, nstart = 100, ...){
     for(i in 1:nstart){
 
     #Se ejecuta el algoritmo
-    km <- kmeans(df, centers = k, ...)
+    km <- kmeans(df, centers = k, nstart = 1,...)
 
     df_aux <- data.frame(k = k, 
-        totss = km$totss, 
+        tot.withinss = km$tot.withinss, 
         tiempo = NA)
-
+    df_kmeans <- rbind(df_kmeans, df_aux)
         if(i==1){ 
-            mejor_optimo <- km$totss
+            mejor_optimo <- km$tot.withinss
             mejor_km <- km
-        }else if(mejor_optimo > km$totss){
-            mejor_optimo <- km$totss
+        }else if(mejor_optimo > km$tot.withinss){
+            mejor_optimo <- km$tot.withinss
             mejor_km <- km
         }
     
     }
-    optimo_promedio <- mean(df_kmeans$totss)
-    atraccion_mejor_optimo <- sum(df_kmeans$totss == mejor_optimo)/nrow(df_kmeans)*100
+    
+    optimo_promedio <- mean(df_kmeans$tot.withinss)
+
+    atraccion_mejor_optimo <- sum(df_kmeans$tot.withinss == mejor_optimo)/nrow(df_kmeans)*100
+
+    #Grafico de los optimos locales
     plot_optimos <- df_kmeans %>%
-        ggplot(aes(x = totss)) +
+        ggplot(aes(x = tot.withinss)) +
         geom_histogram() + 
         theme_minimal() +
         labs(x = "Optimos locales", 
         y = "Cantidad")
 
-    return(list(df_kmeans, 
-        plot_optimos,
-        optimo_promedio,
-        mejor_optimo, 
-        atraccion_mejor_optimo, 
-        mejor_km
+    #Se retorna una lista de parametros de relevancia para el laboratior
+    return(list(df = df_kmeans, 
+        plot_optimos = plot_optimos,
+        optimo_promedio = optimo_promedio,
+        mejor_optimo = mejor_optimo, 
+        atraccion_mejor_optimo = atraccion_mejor_optimo, 
+        mejor_km = mejor_km
         ))
 
 
 }
-fn_punto_1()
-
 
